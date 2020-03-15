800a28637f91233b643048b21514a6685056116e
wooju_lgLGlaptop <yhatemplar@naver.com>
2020-03-15 14:02:50 +0900

    Test Driven Development
    junit 4 using junit 5 (individual import hamcrest)

--- eatgo-api/src/test/java/kr/co/fastcampus/eatgo/domain/RestaurantTests.java
+++ eatgo-api/src/test/java/kr/co/fastcampus/eatgo/domain/RestaurantTests.java
@@ -2,11 +2,22 @@ package kr.co.fastcampus.eatgo.domain;
 
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 class RestaurantTests {
 
     @Test
     public void creation()    {
-        Restaurant restaurant = new Restaurant();
+        Restaurant restaurant = new Restaurant("Bob zip","Seoul");
+        assertThat(restaurant.getName(), is("Bob zip"));
+        assertThat(restaurant.getAddress(), is("Seoul"));
+    }
+
+    @Test
+    public void information(){
+        Restaurant restaurant = new Restaurant("Bob zip","Seoul");
+        assertThat(restaurant.getInformation(),is("Bob zip in Seoul") );
     }
 
 }
