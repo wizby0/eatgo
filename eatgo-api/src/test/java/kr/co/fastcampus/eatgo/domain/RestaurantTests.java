807a9ebfe7879c9bf92bd0c1e493b2803129190c
wooju_lgLGlaptop <yhatemplar@naver.com>
2020-03-15 13:12:29 +0900

    Restaurant initial add

---
+++ eatgo-api/src/test/java/kr/co/fastcampus/eatgo/domain/RestaurantTests.java
@@ -0,0 +1,12 @@
+package kr.co.fastcampus.eatgo.domain;
+
+import org.junit.jupiter.api.Test;
+
+class RestaurantTests {
+
+    @Test
+    public void creation()    {
+        Restaurant restaurant = new Restaurant();
+    }
+
+}
