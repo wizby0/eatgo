99e6552463f3a2754b74ccdc46b9ca0e679c4773
wooju_lgLGlaptop <yhatemplar@naver.com>
2020-03-20 18:23:41 +0900

    RestaurantService added (using menu and Restaurant repository)

--- eatgo-api/src/main/java/kr/co/fastcampus/eatgo/interfaces/RestaurantController.java
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/interfaces/RestaurantController.java
@@ -1,9 +1,7 @@
 package kr.co.fastcampus.eatgo.interfaces;
 
-import kr.co.fastcampus.eatgo.domain.MenuItem;
-import kr.co.fastcampus.eatgo.domain.MenuItemRepository;
+import kr.co.fastcampus.eatgo.application.RestaurantService;
 import kr.co.fastcampus.eatgo.domain.Restaurant;
-import kr.co.fastcampus.eatgo.domain.RestaurantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,26 +13,23 @@ import java.util.List;
 public class RestaurantController {
 
 
-//    private RestaurantRepository repository = new RestaurantRepository();
+    //    private RestaurantRepository repository = new RestaurantRepository();
     @Autowired
-    private RestaurantRepository restaurantRepository;
+    private RestaurantService restaurantService;
 
-    @Autowired
-    private MenuItemRepository menuItemRepository;
 
     @GetMapping("/restaurants")
     public List<Restaurant> list(){
-//        List<Restaurant> restaurants = repository.findAll();
-//        return restaurants;
-        return restaurantRepository.findAll();
+        List<Restaurant> restaurants = restaurantService.getRestaurants();
+        return restaurants;
+//        return restaurantRepository.findAll();
     }
 
     @GetMapping("/restaurants/{id}")
     public Restaurant detail(@PathVariable Long id){
-        Restaurant restaurant = restaurantRepository.findbyId(id);
 
-        List<MenuItem> menuItems = menuItemRepository.findAllByRestaurantId(id);
-        restaurant.setMenuItems(menuItems);
+        Restaurant restaurant = restaurantService.getRestaurant(id);
+
 
         return restaurant;
 
