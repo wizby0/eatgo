c892548771c4f25918e58caffa1786ce6bb39ed3
wooju_lgLGlaptop <yhatemplar@naver.com>
2020-03-20 16:26:09 +0900

    MenuItem repository added

--- eatgo-api/src/main/java/kr/co/fastcampus/eatgo/interfaces/RestaurantController.java
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/interfaces/RestaurantController.java
@@ -1,5 +1,7 @@
 package kr.co.fastcampus.eatgo.interfaces;
 
+import kr.co.fastcampus.eatgo.domain.MenuItem;
+import kr.co.fastcampus.eatgo.domain.MenuItemRepository;
 import kr.co.fastcampus.eatgo.domain.Restaurant;
 import kr.co.fastcampus.eatgo.domain.RestaurantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,19 +17,26 @@ public class RestaurantController {
 
 //    private RestaurantRepository repository = new RestaurantRepository();
     @Autowired
-    private RestaurantRepository repository;
+    private RestaurantRepository restaurantRepository;
+
+    @Autowired
+    private MenuItemRepository menuItemRepository;
 
     @GetMapping("/restaurants")
     public List<Restaurant> list(){
 //        List<Restaurant> restaurants = repository.findAll();
 //        return restaurants;
-        return repository.findAll();
+        return restaurantRepository.findAll();
     }
 
     @GetMapping("/restaurants/{id}")
     public Restaurant detail(@PathVariable Long id){
-//        Restaurant restaurant = repository.findbyId(id);
-//        return restaurant;
-        return repository.findbyId(id);
+        Restaurant restaurant = restaurantRepository.findbyId(id);
+
+        List<MenuItem> menuItems = menuItemRepository.findAllByRestaurantId(id);
+        restaurant.setMenuItems(menuItems);
+
+        return restaurant;
+
     }
 }
