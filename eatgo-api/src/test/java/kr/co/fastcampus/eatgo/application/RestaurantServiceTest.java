6130fb048963e51746003f8fc9d874ad4f32d3d4
wooju_lgLGlaptop <yhatemplar@naver.com>
2020-03-28 10:46:38 +0900

    Lombok -> getter setter added

--- eatgo-api/src/test/java/kr/co/fastcampus/eatgo/application/RestaurantServiceTest.java
+++ eatgo-api/src/test/java/kr/co/fastcampus/eatgo/application/RestaurantServiceTest.java
@@ -44,7 +44,14 @@ public class RestaurantServiceTest {
 
     private void mockRestaurantRepository() {
         List<Restaurant> restaurants = new ArrayList<>();
-        Restaurant restaurant = new Restaurant(1004L,"Bob zip","Seoul");
+//        Restaurant restaurant = new Restaurant(1004L,"Bob zip","Seoul");
+        Restaurant restaurant = Restaurant.builder()
+                .id(1004L)
+                .name("Bob zip")
+                .address("Seoul")
+                .menuItems(new ArrayList<MenuItem>())
+                .build();
+
         restaurants.add(restaurant);
         given(restaurantRepository.findAll()).willReturn(restaurants);
 
@@ -87,11 +94,26 @@ public class RestaurantServiceTest {
 
     @Test
     public void addRestaurant(){
+        given(restaurantRepository.save(any())).will(invocation -> {
+            Restaurant restaurant = invocation.getArgument(0);
+            restaurant.setId(1234L);
+            return restaurant;
+        });
 
-        Restaurant restaurant = new Restaurant("Beryong", "Busan");
-        Restaurant saved = new Restaurant(1234L, "Beryong", "Busan");
+//        Restaurant restaurant = new Restaurant("Beryong", "Busan");
+        Restaurant restaurant = Restaurant.builder()
+                .name("Beryong")
+                .address("Busan")
+                .build();
 
-        given(restaurantRepository.save(any())).willReturn(saved);
+//        Restaurant saved = new Restaurant(1234L, "Beryong", "Busan");
+
+//        Restaurant saved = Restaurant.builder()
+//                .id(1234L)
+//                .name("Beryong")
+//                .address("Busan")
+//                .build();
+
 
         Restaurant created = restaurantService.addRestaurant(restaurant);
 
@@ -101,7 +123,11 @@ public class RestaurantServiceTest {
 
     @Test
     public void updateRestaurant(){
-        Restaurant restaurant = new Restaurant(1004L,"Bob zip","Seoul");
+        Restaurant restaurant = Restaurant.builder()
+                .id(1004L)
+                .name("Bob zip")
+                .address("Seoul")
+                .build();
 
         given(restaurantRepository.findById(1004L))
                 .willReturn(java.util.Optional.of(restaurant));
--- eatgo-api/src/test/java/kr/co/fastcampus/eatgo/interfaces/RestaurantControllerTests.java
+++ eatgo-api/src/test/java/kr/co/fastcampus/eatgo/interfaces/RestaurantControllerTests.java
@@ -13,6 +13,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.core.StringContains.containsString;
@@ -46,7 +47,12 @@ public class RestaurantControllerTests {
     public void list() throws Exception {
 
         List<Restaurant> restaurants = new ArrayList<>();
-        restaurants.add(new Restaurant(1004L,"Bob zip","Seoul"));
+//        restaurants.add(new Restaurant(1004L,"Bob zip","Seoul"));
+        restaurants.add(Restaurant.builder()
+                .id(1004L)
+                .name("Bob zip")
+                .address("Seoul")
+                .build());
 
         given(restaurantService.getRestaurants()).willReturn(restaurants);
 
@@ -65,11 +71,27 @@ public class RestaurantControllerTests {
 
     @Test
     public void detail() throws Exception {
-        Restaurant restaurant1 = new Restaurant(1004L,"JOKER House","Seoul");
-        restaurant1.addMenuItem(new MenuItem("Kimchi"));
+//        Restaurant restaurant1 = new Restaurant(1004L,"JOKER House","Seoul");
+        Restaurant restaurant1 = Restaurant.builder()
+                .id(1004L)
+                .name("JOKER House")
+                .address("Seoul")
+                .build();
 
-        Restaurant restaurant2 = new Restaurant(2020L,"Cyber Food","Seoul");
-        restaurant2.addMenuItem(new MenuItem("Kimchi"));
+        MenuItem menuItem = MenuItem.builder()
+                .name("Kimchi")
+                .build();
+
+        restaurant1.setMenuItems(Arrays.asList(new MenuItem("Kimchi")));
+
+//        Restaurant restaurant2 = new Restaurant(2020L,"Cyber Food","Seoul");
+        Restaurant restaurant2 = Restaurant.builder()
+                .id(1004L)
+                .name("Cyber Food")
+                .address("Seoul")
+                .build();
+
+        restaurant2.setMenuItems(Arrays.asList(new MenuItem("Kimchi")));
 
         given(restaurantService.getRestaurant(1004L)).willReturn(restaurant1);
         given(restaurantService.getRestaurant(2020L)).willReturn(restaurant2);
@@ -104,10 +126,16 @@ public class RestaurantControllerTests {
 
 
     @Test
-    public void create() throws Exception {
+    public void createWithvalidData() throws Exception {
         given(restaurantService.addRestaurant(any())).will(invocation -> {
             Restaurant restaurant = invocation.getArgument(0);
-            return new Restaurant(1234L,restaurant.getName(),restaurant.getAddress());
+
+
+            return Restaurant.builder()
+                    .id(1234L)
+                    .name(restaurant.getName())
+                    .address(restaurant.getAddress())
+                    .build();
 
         });
 
@@ -122,6 +150,30 @@ public class RestaurantControllerTests {
         verify(restaurantService).addRestaurant(any());
     }
 
+    @Test
+    public void createWithinvalidData() throws Exception {
+        given(restaurantService.addRestaurant(any())).will(invocation -> {
+            Restaurant restaurant = invocation.getArgument(0);
+
+
+            return Restaurant.builder()
+                    .id(1234L)
+                    .name(restaurant.getName())
+                    .address(restaurant.getAddress())
+                    .build();
+
+        });
+
+        mvc.perform(post("/restaurants")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"name\":\"Beryong\", \"address\": \"Busan\"}"))
+                .andExpect(status().isBadRequest())
+                .andExpect(header().string("location", "/restaurants/1234"))
+                .andExpect(content().string("{}"))
+                .andDo(print());
+
+        verify(restaurantService).addRestaurant(any());
+    }
 
 
     @Test
